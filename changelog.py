a0b157dbe7383d9d388efcca0f26acb1da3d40f1
rautamak <timi.rautamaki@gmail.com>
2019-11-20 18:16:13 +0100

    scripts: add changelog generator

---
+++ changelog.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+import subprocess
+import os
+
+BUILD_TOP = "/root/caf10/"
+MANIFEST = "../manifest/default.xml"
+CHANGELOG_FILENAME = "../releases/changelog_{}.txt".format(datetime.today().strftime("%Y%m%d_%H%M%S"))
+print("Changelog to file {}".format(CHANGELOG_FILENAME))
+custom_repos = []
+
+with open(MANIFEST, "r") as manifest:
+    m = manifest.readlines()
+
+    for row in m:
+	row = row.strip()
+        if row.startswith("<project name="):
+            custom_repos.append(row.split(" ")[2].split('"')[1])
+
+# Get last changelog to only log changes after it
+dates = []
+for file in os.listdir("../releases/"):
+    if file.endswith(".txt"):
+        date = (file.split("_")[1] + file.split("_")[2]).split(".")[0]
+        dates.append(datetime.strptime(date, "%Y%m%d%H%M%S"))
+
+previous = str(max(dates)).replace(" ", "T")
+
+# Write changelog
+changelog_file = open(CHANGELOG_FILENAME, "w+")
+for repo in custom_repos:
+    full_path = "{}{}".format(BUILD_TOP, repo)
+    log = subprocess.Popen("git log --after={} --pretty='format:%h %<(10)%an %s'".format(previous), shell=True, cwd=full_path, stdout=subprocess.PIPE)
+    output = log.stdout.read()
+    if output != "":
+        changelog_file.write("{}\n\n{}\n\n".format(repo, output))
