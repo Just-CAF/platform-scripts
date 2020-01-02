5ae4caba7cc55d3524953e639c40486471d86454
rautamak <13891897+rautamak@users.noreply.github.com>
2020-01-02 11:53:32 +0100

    scripts: update upstreamer to get repos from manifest

--- upstream-android.py
+++ upstream-android.py
@@ -1,33 +1,27 @@
 import subprocess
 
 TAG = "LA.UM.8.4.r1-04700-8x98.0"
-build_top = "/root/caf10/"
-
-custom_repos = {
-    'build/core': 'platform/build/core',
-    'build/soong': 'platform/build/soong',
-    'external/libunwind_llvm': 'platform/external/libunwind_llvm',
-    'hardware/libhardware': 'platform/hardware/libhardware',
-    'packages/apps/Settings': 'platform/packages/apps/settings',
-    'bionic': 'platform/bionic',
-    'external/selinux': 'platform/external/selinux',
-    'frameworks/av': 'platform/frameworks/av',
-    'system/core': 'platform/system/core',
-    'vendor/qcom/opensource/audio-hal/primary-hal': 'platform/hardware/qcom/audio',
-    'vendor/qcom/opensource/commonsys/cryptfs_hw': 'platform/vendor/qcom-opensource/cryptfs_hw/',
-    'vendor/qcom/opensource/interfaces': 'platform/vendor/qcom-opensource/interfaces/',
-    'hardware/qcom/media': 'platform/hardware/qcom/media',
-    'frameworks/base': 'platform/frameworks/base',
-    'packages/services/Telephony': 'platform/packages/services/telephony',
-    'vendor/codeaurora/commonsys/telephony': 'platform/vendor/codeaurora/commonsys/telephony',
-    'frameworks/opt/telephony': 'platform/frameworks/opt/telephony',
-    'frameworks/opt/net/wifi': 'platform/frameworks/opt/net/wifi',
-    'vendor/qcom/opensource/audio-hal/primary-hal': 'platform/hardware/qcom/audio',
-    'hardware/qcom/media': 'platform/hardware/qcom/media',
-    'packages/apps/Messaging': 'platform/packages/apps/Messaging'
-}
-
 codeaurora_base = "https://source.codeaurora.org/quic/la"
+build_top = "/root/caf10/"
+upstreamable_tag = 'upstream="caf"'
+custom_repos = {}
+
+f = open("manifest/default.xml", "r")
+for line in f.readlines():
+    if upstreamable_tag in line:
+        name = line.split('"')[1].replace("_", "/")
+        path = line.split('"')[3]
+
+
+        # Handling exceptions..
+        if name == "platform/vendor/qcom-opensource/commonsys/cryptfs/hw":
+            name = "platform/vendor/qcom-opensource/cryptfs_hw"
+
+        if name == "platform/external/libunwind/llvm":
+            name = "platform/external/libunwind_llvm"
+
+        print(codeaurora_base + "/" + name)
+        custom_repos[path] = name
 
 for path, repo in custom_repos.items():
     print("repo:", path)
